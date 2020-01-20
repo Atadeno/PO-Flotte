60bccd55b389721ceb99217ca4c05b5c3b839c5a
Valentin <valentin.bielecki@gmail.com>
2020-01-20 15:08:53 +0100

    V3

--- POV3.py
+++ POV3.py
@@ -1,6 +1,5 @@
 import json
 import pandas as pd
-import random
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
@@ -34,7 +33,7 @@ import matplotlib.colors as mcolors
 # jours_Disponibles_Type     une liste de listes des jours disponibles réels pour chaque type de bateaux [0]->Vedettes [1]->Baliseurs [2]->Navires de travaux
 # jours_Disponibles_Moyen    une liste des jours disponibles réels en moyenne pour chaque type de bateaux [0]->Vedettes [1]->Baliseurs [2]->Navires de travaux
 
-print('\n'+'Traitement des fichiers json et csv'+'\n')
+print('\n')
 
 ### LISTE DES PORTS ###
 
@@ -54,7 +53,7 @@ with open('..\data\interventions\interventionsTestMed1_hyp1.json') as json_data:
 
 with open('..\data\BoatSheet0_V5.json') as json_data:
     data_dict_boat = json.load(json_data)
-print(data_dict_boat)
+#print(data_dict_boat)
 
 list_boat_med = []
 for i in range(len(data_dict_boat)):
@@ -67,7 +66,7 @@ for i in range(len(list_boat_med)):
     print(list_boat_med[i]["Port"],list_boat_med[i]["Nature"])
 #print(list_boat_med)
 #print(data_dict[:3])
-print('')
+print('\n')
 
 jours_Disponibles_Type=[[] for i in range(3)] #On fait un tableau pour différencier les types afin de déterminer le nombre de bateau pouvant traiter les interventions de type 1, 2 ou 3
 for d in list_boat_med:
@@ -127,6 +126,7 @@ coord_intervention_1=list_intervention_1 #liste des coordonnées des interventio
 
 
 ### PROPORTION SELON LE TYPE D'INTERVENTION DANS LA MER MEDITERRANEE ###
+
 total=len(list_intervention_1+list_intervention_2+list_intervention_3)
 print('Interventions de niveau 1: ',len(list_intervention_1),round(100*len(list_intervention_1)/total,1),'%')
 print('Interventions de niveau 2: ',len(list_intervention_2),round(100*len(list_intervention_2)/total,1),'%')
@@ -137,11 +137,11 @@ print('\n')
 ### CLUSTER AVEC ALGORITHME K MEAN ###
 
 k=36 #On remarque qu'il y a 3 grandes zones contenant certains ports (Corse, Sète et Marseille et Toulon et Cannes), on décompose en 12 mois et 3 zones soit 36 clusters
-print('Nombre de Clusters:',k)
+print('Nombre de Clusters:', k, '\n')
 
-coord_interventions=coord_intervention_1+coord_intervention_2+coord_intervention_3
+coord_interventions=coord_intervention_3 #Liste de toutes les interventions
 
-kmeans = KMeans(n_clusters=k, random_state=0).fit(np.array(coord_intervention_1+coord_intervention_2+coord_intervention_3))
+kmeans = KMeans(n_clusters=k, random_state=0).fit(np.array(coord_interventions))
 numero_cluster=kmeans.predict(coord_interventions) #On associe à chaque intervention un numéro de cluster
 centres=kmeans.cluster_centers_
 centres=centres.transpose()
@@ -154,7 +154,7 @@ for i in range(len(numero_cluster)):
 #colors=['b','g','orange','c','m','y']
 colors=list(mcolors.TABLEAU_COLORS.values()) #OU CSS4 OU BASE
 cluster_plot=[]
-plt.scatter(centres[0], centres[1],c = 'k', marker = 'o', linewidth = 1)
+#plt.scatter(centres[0], centres[1],c = 'k', marker = 'o', linewidth = 1)
 for i in range(k):
     cluster_plot.append(np.array(valeur_graphique[i]).transpose())
     plt.scatter(cluster_plot[i][0], cluster_plot[i][1],c = colors[i%len(colors)], marker = 'd', linewidth = 0.5)
@@ -162,20 +162,23 @@ for i in range(len(coord_port_med)):
     plt.scatter(coord_port_med[i][0], coord_port_med[i][1],c = 'r', marker = 's', linewidth = 3)
 
 
-cluster=[[] for i in range(k)] #On redéfnie les clusters correctement en une liste d'intervention avec chaque intervention sous forme de dictionnaire
+cluster=[[] for i in range(k)] #On redéfinie les clusters correctement en une liste d'intervention avec chaque intervention sous forme de dictionnaire
 for i in range(len(numero_cluster)):
     for j in range(len(data_dict_intervention)):
         if data_dict_intervention[j].get('coord') == coord_interventions[i]:
             cluster[numero_cluster[i]].append(data_dict_intervention[i])
             break
-
+"""
 for i in range(len(cluster)):
     print('Cluster n°',i+1,len(cluster[i]))
-
-
-### AFFICHAGE MEDITERRANEE AVEC LE TRAIT DE CÔTE ###
-
 """
+plt.show()
+    
+### AFFICHAGE MEDITERRANEE AVEC LE TRAIT DE CÔTE ###   
+    
+"""
+print('\n'+'Représentation des ESM et des Ports sur une Carte'+'\n')
+
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 #setup Lambert Conformal basemap.
@@ -189,59 +192,24 @@ m.drawcoastlines()
 plt.show()
 #plt.savefig('Cluster.png')
 #plt.close()
+         
+#print(kmeans.predict(coord_port_med)) #Association d'un port à son cluster le plus proche
+#print(nom_port_med)
 """
 
-### CALCUL DU NOMBRE K DE CLUSTER ###
-#           
-#print(kmeans.predict(coord_port_med)) #Association d'un port à son cluster le plus proche
-##print(nom_port_med)
+### Répartition des interventions sur les semaines ###
 
-plt.show()
-
-def fromClusterCoordToClusterInterventions():
-    """ clusterCoord (liste de liste d'interventions): la liste des clusters ne contenant que les coordonnÃ©es
-    renvoie les clusters avec toutes les donnÃ©es des interventions, ie une liste de liste d'interventions """
-    clusterInterventions = [[] for i in range(k)]
-    j = 0
-    for num_clus in numero_cluster:
-        for i in range(len(data_dict_intervention)):
-            if data_dict_intervention[i]["coord"] == coord_interventions[j]:
-                clusterInterventions[num_clus].append(data_dict_intervention[i])
-                break
-        j += 1
-    return clusterInterventions
-
-clusterInter = fromClusterCoordToClusterInterventions()
-
-
-nb_semaine = 52
+nb_semaines = 52
 nb_jours_par_semaine = 4
 nb_interventions = len(data_dict_intervention)
-nb_interventions_par_semaine = nb_interventions / nb_semaine
+nb_interventions_par_semaine = nb_interventions / nb_semaines
+
 
 def repartitionAlea(listInter):
-    alea = [[] for i in range(nb_semaine)]
+    alea = [[] for i in range(nb_semaines)] #Renvoie une liste d'interventions par semaine
     listInterRemoved = listInter[:]
-    moy_interventions_par_semaine = len(listInter) // nb_semaine
-    for i in range(nb_semaine):
-        compt = 0
-        listInter = listInterRemoved[:]
-        print(len(listInter))
-        for inter in listInter:
-            if compt == moy_interventions_par_semaine:
-                break
-            elif inter['beginning'] <= i*7 and inter['end'] >= (i+1)*7 - 1:
-                alea[i].append(inter)
-                listInterRemoved.remove(inter)
-                compt += 1
-    return alea
-
-def repartitionAlea1(listInter):
-    alea = [[] for i in range(nb_semaine)]
-    listInterRemoved = listInter[:]
-    moy_interventions_par_semaine = len(listInter) // nb_semaine
     for j in range(len(listInter)):
-        for i in range(nb_semaine):
+        for i in range(nb_semaines):
             listInter = listInterRemoved[:]
             for inter in listInter:
                 if inter['beginning'] <= i*nb_jours_par_semaine and inter['end'] >= (i+1)*nb_jours_par_semaine - 1:
@@ -250,7 +218,21 @@ def repartitionAlea1(listInter):
                     break
     return alea
 
-print(len(clusterInter[0]))
-alea = repartitionAlea1(clusterInter[0])
+alea = repartitionAlea(cluster[1])
+"""
 for l in alea:
     print(len(l), "  ", l)
+""" 
+
+interventions_potentielles_par_bateau = [0, 0, 0, 0, 0, 0]
+interventions_seules_par_bateau = [0, 0, 0, 0, 0, 0]
+for intervention in dict_intervention_type['1']:
+    for j in range(len(intervention['listBoat'])):
+        interventions_potentielles_par_bateau[j]+=intervention['listBoat'][j]
+    if sum(intervention['listBoat'])==1:
+        k = 0
+        while intervention['listBoat'][k]!=1:
+            k+=1
+        interventions_seules_par_bateau[k]+=1
+print(interventions_potentielles_par_bateau)
+print(interventions_seules_par_bateau) 
